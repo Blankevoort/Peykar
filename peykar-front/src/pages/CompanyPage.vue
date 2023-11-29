d02156c25bfd34315970dc14e47d0f66c844f333
Moeen Sedaqati <moeensedaghaty86@gmail.com>
2023-11-29 21:51:48 +0330

    little part of company page completed

--- peykar-front/src/pages/CompanyPage.vue
+++ peykar-front/src/pages/CompanyPage.vue
@@ -11,7 +11,7 @@
                     border-top-right-radius: 10px;
                     border-top-left-radius: 10px;
                   "
-                  src="https://fileapi.jobvision.ir/StaticFiles/Employer/DefaultImages/default-companyHeader.jpeg?v=20231122"
+                  src="https://fileapi.jobvision.ir/api/v1.0/files/getimage?fileid=1240649&width=1440&height=280"
                 />
               </div>
 
@@ -25,27 +25,41 @@
                         <q-item-section avatar>
                           <q-avatar size="95px">
                             <q-img
-                              src="https://fileapi.jobvision.ir/api/v1.0/files/getimage?fileid=154256&width=119&height=119"
+                              src="https://fileapi.jobvision.ir/api/v1.0/files/getimage?fileid=153717&width=119&height=119"
                             />
                           </q-avatar>
                         </q-item-section>
 
                         <q-item-section class="q-pa-md q-ml-xl">
-                          <q-item-label class="text-bold text-h5"
-                            >شرکت تولیدی و بازرگانی رکسان و دارا (سن
-                            ایچ)</q-item-label
-                          >
+                          <q-item-label class="text-bold text-h5 row">
+                            تربت پاک
 
-                          <div class="q-pt-md q-pb-sm">
-                            <span class="q-pr-xs" style="font-size: 16px"
-                              >5</span
+                            <div class="q-px-md">
+                              <span class="q-pr-xs" style="font-size: 16px"
+                                >3.5</span
+                              >
+                              <q-icon
+                                size="24px"
+                                color="orange-4"
+                                name="star"
+                              />
+                            </div>
+                          </q-item-label>
+
+                          <div class="text-grey-7 q-pb-sm q-my-md">
+                            <span
+                              class="q-pr-md text-grey-8"
+                              style="font-size: 12px"
+                              >https://torbatepak.com/</span
                             >
-                            <q-icon size="24px" color="orange-4" name="star" />
+
+                            <span>تا 200 نفر</span>
                           </div>
 
-                          <div class="text-grey-7 q-pb-sm">51 تا 200 نفر</div>
-
-                          <div class="text-grey-7">شرکت تولیدی و بازرگانی</div>
+                          <div class="text-grey-7">
+                            سرمایه گذار و پیمانکار پروژه های ساختمانی، نفت، گاز
+                            و معادن
+                          </div>
                         </q-item-section>
 
                         <q-item-section class="q-pa-sm row q-mr-lg" side>
@@ -72,12 +86,49 @@
             </div>
           </div>
 
-          <div class="col-12 row">
-            <div class="col-6">
-              <div class="q-px-sm"></div>
+          <div class="col-12 row q-my-md">
+            <div class="col-9">
+              <div class="q-px-sm full-width bg-white br-10">
+                <q-tabs
+                  active-class="active-tab"
+                  indicator-color="transparent"
+                  v-model="tab"
+                  align="left"
+                >
+                  <q-tab name="about" label="درباره شرکت" />
+
+                  <q-tab name="opportunity" label="فرصت‌های شغلی (8)" />
+
+                  <q-tab name="charts" label="سازمان از نگاه آمار" />
+
+                  <q-tab name="members" label="معرفی اعضای سازمان" />
+                </q-tabs>
+              </div>
             </div>
-            <div class="col-6 row justify-end">
-              <div class="q-px-sm"></div>
+
+            <div class="col-3 row justify-end">
+              <div class="q-px-sm full-width">
+                <div class="bg-white br-10">
+                  <q-expansion-item
+                    expand-icon-class="text-primary"
+                    class="text-bold"
+                    expand-separator
+                    label="سوابق ارسال رزومه برای این شرکت"
+                  >
+                    <div class="full-width text-center">
+                      <div class="text-grey-6 q-py-lg text-weight-regular">
+                        تاکنون رزومه‌ای برای این شرکت ارسال نکرده اید!
+                      </div>
+
+                      <q-img
+                        width="245px"
+                        height="165px"
+                        src="https://jobvision.ir/assets/images/jobs/no-apply-history.svg"
+                      />
+                    </div>
+                  </q-expansion-item>
+                </div>
+              </div>
             </div>
           </div>
         </div>
@@ -87,18 +138,22 @@
 </template>
 
 <script>
-export default {};
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const tab = ref("about");
+
+    return {
+      tab,
+    };
+  },
+};
 </script>
 
 <style scoped>
 [dir="rtl"] .q-item__section--side {
   padding: 0;
-}
-
-.q-tab__label {
-  font-size: 12px;
-  line-height: 1.715em;
-  font-weight: 500;
 }
 
 .q-badge {
@@ -121,48 +176,31 @@ export default {};
   padding: 0;
 }
 
-.active-tabs {
-  background-color: #5660f2 !important;
-  color: white !important;
-}
-
-.active-tab {
-  border-left: 3px solid;
-  border-color: #5660f2 !important;
-  border-top-left-radius: 0px !important;
-  border-bottom-left-radius: 0px !important;
-  font-weight: bold !important;
-}
-
-.active-sort-type {
-  background-color: #5660f2 !important;
-  color: white !important;
-}
-
-.custom-border {
-  border-radius: 10px !important;
-  border: 2px solid #dde1e6;
-}
-
-.active-category {
-  background: #4a4e57 !important;
-  color: white !important;
-}
-
-.not-active-category {
-  cursor: pointer;
-  border-radius: 40px;
-  background-color: #fff;
-  border: 1px solid #e0e0e0;
-  margin: 0 4px;
-  padding: 8px 16px;
-}
-
 .br-8 {
   border-radius: 8px;
 }
 
 .br-10 {
   border-radius: 10px !important;
+}
+
+.q-badge {
+  background-color: transparent;
+  color: green;
+}
+
+.q-tab-panels {
+  background-color: transparent;
+}
+
+.q-tab-panel {
+  padding: 0;
+}
+
+.active-tab {
+  border-bottom: 3px solid;
+  border-color: #5660f2 !important;
+  font-weight: bold !important;
+  color: #5660f2;
 }
 </style>
